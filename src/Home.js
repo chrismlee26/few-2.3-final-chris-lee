8efcf1276ad25751eb0a81debfaf686e0740cae1
CHRIS_LEE <chrismlee87@gmail.com>
2021-07-13 14:59:12 -0700

    create-react-app + home.js

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      <h1>Chris Lee</h1>
+      <h4>FEW2.3: Single Page Web Applications</h4>
+
+    </div>
+  )
+}
+
+export default Home
