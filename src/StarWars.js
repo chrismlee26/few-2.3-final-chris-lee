c62203d2a4eadbf2d6c8cc0775a8c27db0d33ff8
CHRIS_LEE <chrismlee87@gmail.com>
2021-07-13 16:32:56 -0700

    api call working, need to display

--- src/StarWars.js
+++ src/StarWars.js
@@ -2,18 +2,30 @@ import React, { useState } from 'react'
 
 
 function StarWars() {
+  // Do we need a placeholder value?
   const [id, setId] = useState();
 
-  // something here to validate input 
+  // input validator
+
+  // api call
+  async function getPerson(id) {
+    const res = await fetch(`https://swapi.dev/api/people/${id}/`)
+    const json = await res.json()
+    console.log(json)
+  }
+
 
   return (
-    <div>
+    <div className="starWars">
       <input
         placeholder="Enter ## 1-16, 18-83"
         onChange={(e) => setId(e.target.value)}
         value={id}
       />
-      <button>Search!</button>
+      <button 
+        className="submitButton"
+        onClick={() => getPerson(id)}
+      >Search!</button>
     </div>
   )
 }
